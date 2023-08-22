8d331eb2dc6716cacfd8341cbe9df29d49176cb8
ayesha007 <ayeshanazish007@gmail.com>
2023-08-22 18:31:27 +0500

    added comments

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,76 +1,48 @@
 import { db } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET (request : NextRequest) {
-//     const client = await db.connect();
-
-// try {
-//     await client.sql
-//      `CREATE TABLE User
-//       ( Email varchar(255), Password varchar(255) );`
-
-// } catch (error) {
-//     return NextResponse.json({ error }, {
-//         status: 500,
-//       });
-// }
-//   const user = await client.sql`SELECT * FROM User;`;
-//   return NextResponse.json({user});
-// }
-
-// export async function POST(request: NextRequest) {
-//   const client = await db.connect();
-//   const req = await request.json();
-
-//    console.log("req iss", req);
-
-//     console.log("pet name is", req.userEmail);
-//     console.log("owner is", req.userPassword);
-
-//   try {
-//       await client.sql`INSERT INTO User (Email, Password) VALUES (${req.userEmail}, ${req.userPassword});`;
-//       return NextResponse.json({ message: "User added successfully." });
-//   } catch (error) {
-//       return NextResponse.json({ error }, {
-//           status: 500,
-//       });
-//   }
-// }
-
+// Handle GET requests
 export async function GET(request: NextRequest) {
+  // Connect to the PostgreSQL database
   const client = await db.connect();
- 
+
   try {
+    // Create a table named "User" if it doesn't exist
     await client.sql`CREATE TABLE IF NOT EXISTS "User" (
       "id" serial PRIMARY KEY,
       "Email" varchar(255),
       "Password" varchar(255)
     );`;
-    // const names = ['Cat', 'Max'];
-    
   } catch (error) {
+    // If there's an error, send a 500 status response
     return NextResponse.json({ error }, {
-        status: 500,
-      });
+      status: 500,
+    });
   }
 
-
+  // Fetch all user data from the "User" table
   const user = await client.sql`SELECT * FROM "User"`;
 
-return NextResponse.json({ user });
-  }
+  // Send the user data as a JSON response
+  return NextResponse.json({ user });
+}
 
-export async function POST(request: NextRequest){
+// Handle POST requests
+export async function POST(request: NextRequest) {
+  // Connect to the PostgreSQL database
   const client = await db.connect();
+
+  // Extract JSON data from the incoming request body
   const req = await request.json();
 
-     console.log("req iss", req);
+  // Log the extracted data for debugging purposes
+  console.log("req is", req);
+  console.log("email", req.userEmail);
+  console.log("password", req.userPassword);
 
-    console.log("email", req.userEmail);
-    console.log("password", req.userPassword);
-  
-  
-    await client.sql`INSERT INTO "User" ("Email", "Password") VALUES (${req.userEmail}, ${req.userPassword});`;
-    return NextResponse.json(req);
-    
+  // Insert a new user into the "User" table using the extracted data
+  await client.sql`INSERT INTO "User" ("Email", "Password") VALUES (${req.userEmail}, ${req.userPassword});`;
+
+  // Send the extracted data as a JSON response
+  return NextResponse.json(req);
 }
